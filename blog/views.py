5e89e3ba9ab6fa9b8fa04d5175e7172deb207166
Your Name <you@example.com>
2016-05-24 11:41:18 -0400

    Added users and user pages

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
 from django.shortcuts import redirect
-
+from django.contrib.auth.models import User
+from .models import Post
+from .forms import PostForm
+  
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -12,20 +13,16 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
+def user_post_list(request, pk):
+    posts = Post.objects.filter(author__pk=pk, published_date__lte=timezone.now()).order_by('published_date')
+    author = User.objects.get(pk=pk)
+    return render(request, 'blog/user_detail.html', {'posts': posts, 'author': author})
+     
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-    
+     form = PostForm()
+     return render(request, 'blog/post_edit.html', {'form': form})
+     
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
