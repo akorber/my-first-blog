8786bb22d49b6e3eaf9ca40a612f7c3c7c3591b9
Your Name <you@example.com>
2016-06-06 11:37:51 -0400

    the login thingy

--- blog/views.py
+++ blog/views.py
@@ -5,24 +5,48 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from .models import Post
 from .forms import PostForm
-  
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
+    
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
+    
+@login_required
 def user_post_list(request, pk):
     posts = Post.objects.filter(author__pk=pk, published_date__lte=timezone.now()).order_by('published_date')
     author = User.objects.get(pk=pk)
     return render(request, 'blog/user_detail.html', {'posts': posts, 'author': author})
-     
+    
+@login_required
 def post_new(request):
      form = PostForm()
      return render(request, 'blog/post_edit.html', {'form': form})
-     
+
+@login_required
+def post_draft_list(request):
+	 posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	 return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+	post = get_objet_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('blog.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('blog.view.post_list')
+
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
